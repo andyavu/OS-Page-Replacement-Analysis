1ba3a2fd43d415c3911f25efa210dceb83fde72a
Andy Vu <andyavu@gmail.com>
2021-04-26 15:41:58 -0700

    project set up

---
+++ src/cs4310hw2/CS4310Hw2.java
@@ -0,0 +1,17 @@
+//******************************************************************************
+// Author      : Andy Vu
+// Project     : Project 2
+// Course      : CS 4310
+// File        : CS4310Hw2.java
+// Description : Simulates page replacement manager and does performance analysis
+//******************************************************************************
+
+package cs4310hw2;
+
+public class CS4310Hw2 
+{
+    public static void main(String args[]) 
+    {
+        
+    }
+}
